25214c862b7b78b489998ff845990b7b427425e1
Dounia <douniakhayar6@gmail.com>
2026-02-18 22:02:25 +0000

    Initial commit - structure projet Interactive Generative Studio

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+from art.art1 import generate_art1
+from art.art2 import generate_art2
+from art.art3 import generate_art3
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/art1', methods=["GET", "POST"])
+def art1_view():
+    image = None
+    if request.method == "POST":
+        num_shapes = int(request.form["num_shapes"])
+        palette = request.form.get("palette", "random")
+        image = generate_art1(num_shapes, palette)
+    return render_template("art1.html", image=image)
+
+@app.route('/art2', methods=["GET", "POST"])
+def art2_view():
+    image = None
+    if request.method == "POST":
+        num_shapes = int(request.form["num_shapes"])
+        image = generate_art2(num_shapes)
+    return render_template("art2.html", image=image)
+
+@app.route('/art3', methods=["GET", "POST"])
+def art3_view():
+    image = None
+    if request.method == "POST":
+        num_shapes = int(request.form["num_shapes"])
+        palette = request.form.get("palette", "random")
+        image = generate_art3(num_shapes, palette)
+    return render_template("art3.html", image=image)
+
+if __name__ == "__main__":
+    app.run(debug=True)
